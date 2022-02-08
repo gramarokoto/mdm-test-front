7ee34bb4e563fda592eb806a58f23c6d88b92253
gramarokoto <47677136+gramarokoto@users.noreply.github.com>
2022-02-08 19:31:01 +0100

    feat: Add Cart View and Cart Component

---
+++ front/src/components/Cart.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="cart">
+    <h1>Mon Panier</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Cart',
+};
+</script>
---
+++ front/src/views/Cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Cart />
+  </div>
+</template>
+
+<script>
+import Cart from '@/components/Cart.vue';
+
+export default {
+  name: 'CartView',
+  components: {
+    Cart,
+  },
+};
+</script>
